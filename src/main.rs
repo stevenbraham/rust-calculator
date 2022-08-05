b926a29154e6db3ee8c26e6cb5346c9cb801bb04
Steven Braham <steven@braham.biz>
2022-08-05 19:42:10 +0200

    V1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 4 {
+        // stop execution if there is are not enough arguments
+        eprintln!("Supply 3 arguments: [num1] [operation] [num2]");
+        return;
+    }
+
+    let number1: f64 = match (&args[1]).parse() {
+        Ok(n) => n,
+        Err(_) => {
+            eprintln!("First argument is an invalid number");
+            return;
+        }
+    };
+
+    let number2: f64 = match (&args[3]).parse() {
+        Ok(n) => n,
+        Err(_) => {
+            eprintln!("Third argument is an invalid number");
+
+            return;
+        }
+    };
+
+    let operation = &args[2];
+
+    return match operation.as_str() {
+        "+" => println!("{}", number1 + number2),
+        "-" => println!("{}", number1 - number2),
+        "*" => println!("{}", number1 * number2),
+        "/" => println!("{}", number1 / number2),
+        _ => eprintln!("Invalid operation"),
+    };
 }
